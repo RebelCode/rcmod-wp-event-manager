9a23960835a060984bd28ef81ef5daada476c1e6
Miguel Muscat <miguelmuscat93@gmail.com>
2018-03-27 12:20:23 +0200

    Added module main file

---
+++ module.php
@@ -0,0 +1,8 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use RebelCode\EventManager\Module\WpEventManagerModule;
+
+return function(ContainerInterface $c) {
+    return new WpEventManagerModule($c->get('container-factory'));
+};
